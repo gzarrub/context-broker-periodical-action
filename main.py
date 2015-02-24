a363dcd04aea397deff25f636d5ddf7fd46236b9
gzarrub <g.zarrub@gmail.com>
2015-02-24 14:24:15 +0100

    update data test

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import functions as f
+import test as t
 import time
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
-def periodical_action():
-
-    o = f.ContextBroker('http://130.206.85.12:1026')
-
-
-    print o.discover_context_availability().json()
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(periodical_action, 'interval', seconds=5)
+    scheduler.add_job(t.action, 'interval', seconds=60)
     scheduler.start()
 
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
--- test.py
+++ test.py
@@ -3,48 +3,50 @@ import functions as f
 import requests
 import pyproj
 
-o = f.ContextBroker('http://130.206.85.12:1026')
 
-headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-response = requests.get('http://datos.santander.es/api/rest/datasets/control_flotas_posiciones.json', headers=headers)
+def action():
+    o = f.ContextBroker('http://130.206.85.12:1026')
 
-epsg3857 = pyproj.Proj(init='epsg:3857')
-wgs84 = pyproj.Proj(init='EPSG:4326')
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    response = requests.get('http://datos.santander.es/api/rest/datasets/control_flotas_posiciones.json', headers=headers)
 
-for i in range(len(response.json()['resources'])):
-    entity_id = "urn:x-iot:smartsantander:ayto:vehicle:%s:%s" % (response.json()['resources'][i]['ayto:vehiculo'],response.json()['resources'][i]['ayto:indice'])
-    o.entity.entity_add(entity_id,'santander:ayto:vehicle')
+    epsg3857 = pyproj.Proj(init='epsg:3857')
+    wgs84 = pyproj.Proj(init='EPSG:4326')
 
-    for attrib in response.json()['resources'][i]:
+    for i in range(len(response.json()['resources'])):
+        entity_id = "urn:x-iot:smartsantander:ayto:vehicle:%s:%s" % (response.json()['resources'][i]['ayto:vehiculo'],response.json()['resources'][i]['ayto:indice'])
+        o.entity.entity_add(entity_id,'santander:ayto:vehicle')
 
-        if attrib == 'ayto:geolon':
+        for attrib in response.json()['resources'][i]:
 
-            pos = pyproj.transform(epsg3857,wgs84, float(response.json()['resources'][i]['ayto:geolon']),float(response.json()['resources'][i]['ayto:geolat']))
-            position = "%s,-%s" %(str(pos[0]), str(pos[1]))
-            o.entity.attribute.attribute_add('position','coords',value=position)
-            o.entity.attribute.metadata.metadata_add('location', 'string', 'WGS84')
-            o.entity.attribute.add_metadatas_to_attrib('position')
-            o.entity.attribute.metadata.metadata_list_purge()
+            if attrib == 'ayto:geolon':
 
-        elif attrib == 'geom2d:x':
+                pos = pyproj.transform(epsg3857,wgs84, float(response.json()['resources'][i]['ayto:geolon']),float(response.json()['resources'][i]['ayto:geolat']))
+                position = "%s,-%s" %(str(pos[0]), str(pos[1]))
+                o.entity.attribute.attribute_add('position','coords',value=position)
+                o.entity.attribute.metadata.metadata_add('location', 'string', 'WGS84')
+                o.entity.attribute.add_metadatas_to_attrib('position')
+                o.entity.attribute.metadata.metadata_list_purge()
 
-            pos = pyproj.transform(epsg3857,wgs84, float(response.json()['resources'][i]['geom2d:y']),float(response.json()['resources'][i]['geom2d:x']))
-            position = "%s,%s" %(str(pos[0]), str(pos[1]))
-            o.entity.attribute.attribute_add('area','coords',value=position)
-            o.entity.attribute.metadata.metadata_add('area_location', 'string', 'WGS84')
-            o.entity.attribute.add_metadatas_to_attrib('area')
-            o.entity.attribute.metadata.metadata_list_purge()
+            elif attrib == 'geom2d:x':
 
-        elif attrib in ['ayto:instante', 'dc:modified']:
-            o.entity.attribute.attribute_add(attrib,'urn:x-ogc:def:trs:IDAS:1.0:ISO8601',value=response.json()['resources'][i][attrib])
+                pos = pyproj.transform(epsg3857,wgs84, float(response.json()['resources'][i]['geom2d:y']),float(response.json()['resources'][i]['geom2d:x']))
+                position = "%s,%s" %(str(pos[0]), str(pos[1]))
+                o.entity.attribute.attribute_add('area','coords',value=position)
+                o.entity.attribute.metadata.metadata_add('area_location', 'string', 'WGS84')
+                o.entity.attribute.add_metadatas_to_attrib('area')
+                o.entity.attribute.metadata.metadata_list_purge()
 
-        elif attrib in ['ayto:vehiculo', 'ayto:indice', 'geom2d:y', 'ayto:geolat']:
-            pass
-        else:
-            attrib_type = 'string'
-            o.entity.attribute.attribute_add(attrib, attrib_type, value=response.json()['resources'][i][attrib])
+            elif attrib in ['ayto:instante', 'dc:modified']:
+                o.entity.attribute.attribute_add(attrib,'urn:x-ogc:def:trs:IDAS:1.0:ISO8601',value=response.json()['resources'][i][attrib])
 
-    o.entity.add_attributes_to_entity(entity_id)
-    o.entity.attribute.attribute_list_purge()
+            elif attrib in ['ayto:vehiculo', 'ayto:indice', 'geom2d:y', 'ayto:geolat']:
+                pass
+            else:
+                attrib_type = 'string'
+                o.entity.attribute.attribute_add(attrib, attrib_type, value=response.json()['resources'][i][attrib])
 
-print o.update_context()
+        o.entity.add_attributes_to_entity(entity_id)
+        o.entity.attribute.attribute_list_purge()
+
+    print o.update_context()
